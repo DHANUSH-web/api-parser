a20bb5ec47f478640ff0b72423dd17a6ed5c2297
ddhanushhv <ddhanushhv@gmail.com>
2024-07-08 20:12:16 +0000

    Special files from Project IDX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import '@radix-ui/themes/styles.css';
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <meta httpEquiv="Permission-Policy" content="clipboard-write=(self)" />
+      <body className={inter.className}>
+        <Theme>
+        {children}
+          <ThemePanel />
+        </Theme>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-'use client';
+import ApiFetcher from "@/components/fetcher";
 
-import { DownloadIcon, Loader2 } from "lucide-react";
-import React from "react";
-
-export default function Home() {
-  const [isDownloadComplete, setDownloadTimeout] = React.useState(true);
-
-  const handleDownloadEvent = () => {
-    setDownloadTimeout(false);
-    setTimeout(() => {
-      setDownloadTimeout(true);
-    }, 3000);
-  }
-
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={handleDownloadEvent} className="flex items-center justify-center min-w-28 gap-2 text-sm font-medium bg-black text-white px-4 py-2 rounded-md active:scale-75 duration-200">
-        {isDownloadComplete ? "Download" : <Loader2 size={20} className="animate-spin" />}
-      </button>
+    <main className="flex flex-col items-center justify-between min-h-screen p-5">
+      <ApiFetcher />
     </main>
   );
 }
